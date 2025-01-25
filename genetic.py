c5c49beddc7eb6277d73379ee8278892e519d08e
GMar25 <gagemariano@gmail.com>
2025-01-25 14:08:52 -0600

    Add base class structure

--- genetic.py
+++ genetic.py
@@ -1,1 +1,28 @@
-print("hello world! we like $$$$")
+class Person:
+
+    def __init__(self):
+        super()
+
+        self.id = 0
+        self.seat = (0, 0) # Row, column (0, 0 is front left)
+
+        self.pref = \
+            [
+                (0, []), # bool, people
+                (0, 0),  # bool, section vertical 
+                (0, 0),  # bool, section horizontal [0, 2]: window, aisle, other
+            ] 
+
+
+class Seating:
+
+    def __init__(self):
+        super()
+
+        self.arr = [] # array of Persons
+        self.score = 0 # heurestic result
+
+
+
+
+    
